0ddef250fa69b88e80620fdfdedd2a067ee81372
Travis Wright <twright@microsoft.com>
2017-04-24 10:22:32 +0900

    Couple of tweaks

---
+++ BackupRestore/RestoreWideWorldImporters.sql
@@ -0,0 +1,8 @@
+RESTORE FILELISTONLY FROM DISK = '/backup/wwi.bak'
+
+RESTORE DATABASE WideWorldImporters FROM DISK = '/backup/wwi.bak'
+WITH 
+    MOVE 'WWI_Primary' TO '/var/opt/mssql/data/WideWorldImporters.mdf',
+    MOVE 'WWI_UserData' TO '/var/opt/mssql/data/WideWorldImporters_UserData.ndf',
+    MOVE 'WWI_Log' TO '/var/opt/mssql/log/WideWorldImporters.ldf',
+    MOVE 'WWI_InMemory_Data_1' TO '/var/opt/mssql/data/WideWorldImporters_InMemory_Data_1'
