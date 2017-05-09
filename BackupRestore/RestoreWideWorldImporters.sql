4963f899a008fb800cf91ad602c72c8dbae7c9e0
Travis Wright <twright@microsoft.com>
2017-05-09 09:53:41 -0700

    Adding some more test/demo stuff to restore script

--- BackupRestore/RestoreWideWorldImporters.sql
+++ BackupRestore/RestoreWideWorldImporters.sql
@@ -6,3 +6,17 @@ WITH
     MOVE 'WWI_UserData' TO '/var/opt/mssql/data/WideWorldImporters_UserData.ndf',
     MOVE 'WWI_Log' TO '/var/opt/mssql/log/WideWorldImporters.ldf',
     MOVE 'WWI_InMemory_Data_1' TO '/var/opt/mssql/data/WideWorldImporters_InMemory_Data_1'
+
+SELECT Name FROM sys.databases
+
+USE WideWorldImporters
+
+SELECT * FROM Application.Cities
+
+INSERT INTO Application.Cities 
+    (CityName,StateProvinceID, LastEditedBy)
+    VALUES ('NYC',1, 1)
+
+SELECT * FROM Application.Cities ORDER BY CityID DESC
+
+DELETE FROM Application.Cities WHERE CityName = 'NYC'
